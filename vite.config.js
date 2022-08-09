d9658e73ac50c717f12c62b48131072b6e1d046d
meve404 <procontact325@gmail.com>
2022-08-09 11:43:39 -0500

    adding base

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://meve404.github.io/', //to tell the github where the project is/
   plugins: [react()]
 })
